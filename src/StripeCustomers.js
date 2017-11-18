cfaa606f8a0d3bc9217ff221944423706244a37b
Daniel Woelfel <dwwoelfel@gmail.com>
2017-11-17 21:41:56 -0800

    working pagination

--- src/StripeCustomers.js
+++ src/StripeCustomers.js
@@ -3,8 +3,8 @@ import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
 
 const query = gql`
-  query StripeCustomersQuery($cursor: String) {
-    stripeCustomers(after: $cursor) {
+  query StripeCustomersQuery($cursor: String, $limit: Int) {
+    stripeCustomers(after: $cursor, limit: $limit) {
       edges {
         node {
           id
@@ -20,12 +20,87 @@ const query = gql`
   }
 `;
 
-class StripeCustomers extends React.Component {
+class StripeCustomer extends React.Component {
   render() {
-    console.log(this.props.data);
-    return <div>Stripe Customers</div>
+    const {customer} = this.props;
+    return <div>{customer.description}</div>;
   }
 }
 
-const StripeCustomersWithData = graphql(query)(StripeCustomers);
+class StripeCustomers extends React.Component {
+  render() {
+    console.log(this.props.data);
+    if (this.props.data.loading) {
+      return <div>Loading</div>;
+    }
+    if (this.props.data.error) {
+      return <div>Error :( {this.props.data.error.message}</div>;
+    }
+    return (
+      <div>
+        <h1>Stripe Customers</h1>
+        {this.props.data.stripeCustomers.edges.map(s => (
+          <StripeCustomer key={s.node.id} customer={s.node} />
+        ))}
+        {this.props.data.stripeCustomers.pageInfo.hasNextPage ? (
+          <button onClick={this.props.data.loadMoreEntries}>Load More</button>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+const StripeCustomersWithData = graphql(query, {
+  options: {variables: {limit: 2, cursor: null}},
+  props({data: {loading, stripeCustomers, fetchMore, variables}}) {
+    console.log({variables});
+    return {
+      data: {
+        loading,
+        stripeCustomers,
+        loadMoreEntries: () => {
+          const cursor = stripeCustomers.pageInfo.endCursor;
+          return fetchMore({
+            query,
+            variables: {
+              limit: 2,
+              cursor,
+            },
+            updateQuery: (previousResult, {fetchMoreResult, variables}) => {
+              const newEdges = fetchMoreResult.stripeCustomers.edges;
+              const pageInfo = fetchMoreResult.stripeCustomers.pageInfo;
+              const lastEdge =
+                previousResult.stripeCustomers.edges[
+                  previousResult.stripeCustomers.edges.length - 1
+                ];
+              if (lastEdge.node.id !== cursor) {
+                console.error(
+                  'bad pagination query, throwing away results',
+                  lastEdge.node.id,
+                  cursor,
+                );
+                return previousResult;
+              }
+              return newEdges.length
+                ? {
+                    // Put the new comments at the end of the list and update `pageInfo`
+                    // so we have the new `endCursor` and `hasNextPage` values
+                    ...fetchMoreResult,
+                    stripeCustomers: {
+                      ...fetchMoreResult.stripeCustomers,
+                      edges: [
+                        ...previousResult.stripeCustomers.edges,
+                        ...newEdges,
+                      ],
+                    },
+                  }
+                : previousResult;
+            },
+          });
+        },
+      },
+    };
+  },
+})(StripeCustomers);
+
 export default StripeCustomersWithData;
