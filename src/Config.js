ed588a88fbe6e9b128546b20463c71795a1bfc23
Daniel Woelfel <dwwoelfel@gmail.com>
2017-11-21 21:38:04 -0800

    auto-deploy

--- src/Config.js
+++ src/Config.js
@@ -6,9 +6,15 @@ const pages = [
   {id: 'charges', title: 'Charges', component: StripeCharges},
 ];
 
+const DEV = process.env.NODE_ENV === 'development';
+
 const config = {
-  applicationId: '135be0f7-becf-46fe-8a82-bf572606b468', //'0b33e830-7cde-4b90-ad7e-2a39c57c0e11',
-  authToken: '65b25367-8f55-43b2-9ce5-72b4c3014b43', //'2bf7b30e-259e-44b4-b180-05df8f9dbd74',
+  applicationId: DEV
+    ? '135be0f7-becf-46fe-8a82-bf572606b468'
+    : '22de62ff-4c25-49a2-b06c-fbe355aec76a';
+  authToken: DEV
+    ? '65b25367-8f55-43b2-9ce5-72b4c3014b43'
+    : '687b9599-781f-4dc7-9025-7246b9a02953';
   oneGraphUrl: 'https://serve.onegraph.io/dynamic',
   pages,
 };
