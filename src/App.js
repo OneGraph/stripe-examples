a954419d5efade47c9c0ce7ee56569ffd8042e9c
Sean Grove <s@bushi.do>
2018-01-21 17:31:24 -0800

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -62,23 +62,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-<footer className="main-footer">
-  <div className="container-fluid">
-    <div className="row">
-      <div className="col-sm-6">
-        <p>Your company © 2017-2019</p>
-      </div>
-      <div className="col-sm-6 text-right">
-        <p>
-          Design by{' '}
-          <a
-            href="https://bootstrapious.com/admin-templates"
-            className="external">
-            Bootstrapious
-          </a>
-        </p>
-      </div>
-    </div>
-  </div>
-</footer>;
